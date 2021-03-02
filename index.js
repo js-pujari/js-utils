523acec86cd4597adfa9395f044cd82ae6b7e569
js-pujari <79906338+js-pujari@users.noreply.github.com>
2021-03-02 15:40:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const findAvg = (arr) => arr.reduce((p, c) => p + c, 0) / arr.length;
+
 const toCamelCase = (str) => {
     return str
         .replace(/\s(.)/g, ($1) => $1.toUpperCase())
         .replace(/\s/g, ' ')
         .replace(/^(.)/, ($1) => $1.toUpperCase());
 };
+
 const getPercentage = (total, value) => {
     return (total && value) ? Math.round((value / total) * 100) : 0;
 };
+
 const formatPriceINR = (price, isDecimalRequired = true, isRSRequried = true) => {
     if (isDecimalRequired && isRSRequried) {
         return 'Rs. ' + (price || 0).toFixed(2).replace(/(\d)(?=(\d{2})+\d\.)/g, '$1,');
@@ -21,6 +24,7 @@ const formatPriceINR = (price, isDecimalRequired = true, isRSRequried = true) =>
         return 'Rs. ' + (price || 0).toString().replace(/\B(?=(?:(\d\d)+(\d)(?!\d))+(?!\d))/g, ',');
     }
 };
+
 const getMobileFormat = (mobile) => {
     if (mobile && typeof mobile === 'string') {
         mobile = mobile.toString();
@@ -28,7 +32,8 @@ const getMobileFormat = (mobile) => {
     }
     return '';
 };
-const GetFirstElement = (array) => {
+
+const getFirstElement = (array) => {
     if (array && array.length > 0) {
         return array[0];
     } else {
